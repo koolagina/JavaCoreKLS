4516ab5eac643b944d01714a7db1d4dea835bf4c
koolagina <koolagina@gmail.com>
2022-12-06 20:03:21 +0500

    HW1

--- src/HW1/TeamMate.java
+++ src/HW1/TeamMate.java
@@ -1,7 +1,6 @@
 package HW1;
 //создаем класс члена команды
 public class TeamMate {
-
     private String name;
     private int runningSpeed; //скорость спорсмена
     private int archeryAccuracy; //точность спорсмена
